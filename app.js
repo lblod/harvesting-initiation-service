7006486434fbb1c4419a5db9a85d26e849c472eb
Dries Hugaerts <dries.hugaerts@live.be>
2020-05-15 14:02:01 +0200

    created first task creation flow

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 import { app, errorHandler } from 'mu';
+import {createTask} from "./rdflib";
 
 app.get('/', function( req, res ) {
   res.send('Hello harvesting-initiation-service');
-} );
+});
+
+app.get('/initiate', async function(req, res){
+  try{
+    initiateHarvest();
+    res.status(200).send().end();
+  }catch (e) {
+    console.error(e);
+    res.status(400).send().end();
+  }
+
+});
+
+async function initiateHarvest() {
+ console.log("-- initiating harvest --")
+ const publications = await getPublications();
+
+ console.log("-- creating tasks --")
+ for(let publication of publications) {
+   await createTask(publication);
+ }
+}
+
+async function getPublications() {
+  return [
+    'https://publicatie.gelinkt-notuleren.vlaanderen.be/Genk/Gemeente'
+  ]
+}
 
 app.use(errorHandler);
---
+++ rdflib.js
@@ -0,0 +1,81 @@
+import {uuid, sparqlEscapeUri, sparqlEscapeString, sparqlEscapeDateTime} from 'mu';
+import {querySudo as query, updateSudo as update} from '@lblod/mu-auth-sudo';
+
+// TODO add constants
+const CREATOR = 'http://lblod.data.gift/services/harvesting-initiation-service';
+
+const TASK_URI_BASE = 'http://data.lblod.info/id/harvesting-task/';
+const TASK_READY_STATUS = 'http://lblod.data.gift/harvesting-statuses/ready-for-collecting';
+
+const COLLECTION_URI_BASE = 'http://data.lblod.info/id/harvesting-collection/';
+const COLLECTION_NOT_STARTED_STATUS = 'http://lblod.data.gift/collecting-statuses/not-started';
+
+const REMOTE_URI_BASE = 'http://data.lblod.info/id/remote-data-objects/';
+const REMOTE_READY_STATUS = 'http://lblod.data.gift/file-download-statuses/ready-to-be-cached';
+
+// TODO add all prefixes
+const PREFIXES = `
+  PREFIX harvesting: <http://lblod.data.gift/vocabularies/harvesting/>
+  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+  PREFIX dct: <http://purl.org/dc/terms/>
+  PREFIX adms: <http://www.w3.org/ns/adms#>
+  PREFIX prov: <http://www.w3.org/ns/prov#>
+  PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+  PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+  PREFIX rpioHttp: <http://redpencil.data.gift/vocabularies/http/>
+  PREFIX http: <http://www.w3.org/2011/http#>
+`;
+
+export async function createTask(location) {
+  // Shared properties
+  const timestamp = new Date();
+
+  // Harvesting Task properties
+  const taskUUID = uuid();
+  const taskURI = `${TASK_URI_BASE}${taskUUID}`
+
+  // Harvesting Collection properties
+  const collectionUUID = uuid();
+  const collectionURI = `${COLLECTION_URI_BASE}${collectionUUID}`;
+
+  // Remote properties
+  const remoteUUID = uuid();
+  const remoteURI = `${REMOTE_URI_BASE}${remoteUUID}`;
+
+  await update(`
+  ${PREFIXES}
+  INSERT DATA {
+    GRAPH <http://mu.semte.ch/graphs/public> {
+    
+        ${sparqlEscapeUri(taskURI)} a harvesting:HarvestingTask;
+                               mu:uuid ${sparqlEscapeString(taskUUID)};
+                               dct:creator ${sparqlEscapeUri(CREATOR)};
+                               adms:status ${sparqlEscapeUri(TASK_READY_STATUS)};
+                               dct:created ${sparqlEscapeDateTime(timestamp)};
+                               dct:modified ${sparqlEscapeDateTime(timestamp)};
+                               prov:generated ${sparqlEscapeUri(collectionURI)}.
+                               
+        ${sparqlEscapeUri(collectionURI)} a harvesting:HarvestingCollection;
+                               mu:uuid ${sparqlEscapeString(collectionUUID)};
+                               dct:creator ${sparqlEscapeUri(CREATOR)};
+                               adms:status ${sparqlEscapeUri(COLLECTION_NOT_STARTED_STATUS)};
+                               dct:hasPart ${sparqlEscapeUri(remoteURI)} .
+                               
+        ${sparqlEscapeUri(remoteURI)} a nfo:RemoteDataObject, nfo:FileDataObject;
+                                  rpioHttp:requestHeader <http://data.lblod.info/request-headers/accept/text/html>;
+                                  mu:uuid ${sparqlEscapeString(remoteUUID)};
+                                  nie:url ${sparqlEscapeUri(location)};
+                                  dct:creator ${sparqlEscapeUri(CREATOR)};
+                                  adms:status ${sparqlEscapeUri(REMOTE_READY_STATUS)};
+                                  dct:created ${sparqlEscapeDateTime(timestamp)};
+                                  dct:modified ${sparqlEscapeDateTime(timestamp)}.
+
+        <http://data.lblod.info/request-headers/accept/text/html> a http:RequestHeader;
+                                                                    http:fieldValue "text/html";
+                                                                    http:fieldName "Accept";
+                                                                    http:hdrName <http://www.w3.org/2011/http-headers#accept>.                                       
+    }                      
+  }
+  `);
+
+}
