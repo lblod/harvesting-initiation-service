d0829d400481d34087aec4cad8cdcab28eb10545
Oscar <lagartoverde97@gmail.com>
2020-09-11 16:17:41 +0200

    Specify what's left on the script

--- scripts/file.sh
+++ scripts/file.sh
@@ -6,4 +6,7 @@ do
     echo $file
     response=$(curl -F file=@$file -H "X-Rewrite-URL: test" http://file/files | jq '.data.id')
     echo $response
+    # TODO:
+    #  - Construct query that creates a harvesting task with the ids on the response variable
+    #  - Send that query to virtuoso
 done
